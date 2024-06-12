9bab322b613c773102fc36a6d005b03c59646f3c
samantha-hansen <sammuhlbach@gmail.com>
2024-06-11 17:47:35 -0700

    Compile tests in test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,56 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+import os
+from ml.data import process_data
+from ml.model import train_model
+from sklearn.linear_model import LogisticRegression
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+# Implement the first test.
+
+@pytest.fixture
+def load_data():
     """
-    # add description for the first test
+    Load the dataset
     """
-    # Your code here
-    pass
+    project_path = os.getcwd()
+    data_path = os.path.join(project_path, "data", "census.csv")
+    data = pd.read_csv(data_path)
+    return data
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+# Implement the second test.
+def dataset_info():
     """
-    # add description for the second test
+    Test to see dataset form and size
+
+    Parameters:
+        load_data: Census dataset
     """
-    # Your code here
-    pass
+    assert isinstance(data, pd.DataFrame), "Data is not a pandas Dataframe"
+    assert data.shape[0] > 0, "Data does not contain any rows"
+    assert data.shape[1] > 0, "Data does not contain any columns"
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+# Implement the third test.
+@pytest.fixture
+def test_data_process(load_data):
     """
-    # add description for the third test
+    Tests data processing
     """
-    # Your code here
-    pass
+    data = load_data
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        data, categorical_features = cat_features, label='salary', training=True
+    )
+    return X_train, y_train, encoder, lb
