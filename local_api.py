41efdde5c941c49677f3bb1e5ee469b6b6e67390
samantha-hansen <sammuhlbach@gmail.com>
2024-06-12 13:03:48 -0700

    Step 4: API

--- local_api.py
+++ local_api.py
@@ -2,13 +2,13 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = # Your code here
+# Send a GET using the URL http://127.0.0.1:8000
+r = requests.get("http://127.0.0.1:8000")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+# Print the status code
+print(f"Status Code: {r.status_code}")
+# Print the welcome message
+print(f"Message: {r.json()}")
 
 
 
@@ -29,10 +29,10 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = # Your code here
+# Send a POST using the data above
+r = requests.post("http://127.0.0.1:8000/data/", json = data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+# Print the status code
+print(f"Status Code: {r.status_code}")
+# Print the result
+print(f"Result: {r.json()['result']}")
